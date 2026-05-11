c7b085aefa1a512b54434d59889ff5518bdd188a
Swarnim Agrawal <143487092+Swarnim7104@users.noreply.github.com>
2026-05-12 01:06:16 +0530

    Rename TwoRayModel (1).m to TwoRayModel.m

--- TwoRayModel (1).m
+++
@@ -1,2 +0,0 @@
-d=1:1e7; l=sqrt(d.^2+35^2); r=sqrt(d.^2+65^2); lam=1/3; % Params
-plot(10*log10(d), 10*log10((lam/(4*pi))^2*abs(1./l-exp(1j*6*pi*(r-l))./r).^2)); xlabel('Dist'); ylabel('Power'); % Plot
